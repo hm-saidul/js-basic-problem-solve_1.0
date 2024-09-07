485fa6805afd59094277d2eefe580c0ac44aa33f
saidzayan <saidulzayan@gmail.com>
2024-09-07 09:30:27 +0600

    first commit

---
+++ solve.js
@@ -0,0 +1,62 @@
+//1.Create a variable called carName, assign the value Volvo to it.
+let carName = 'toyota';
+
+//2.On one single line, declare three variables with the following names and values:
+// let firstName = 'John';
+// let lastName = 'Doe';
+// age = 35;
+
+//3.
+// let x = 10;
+// y = 5;
+// x = x * y;
+// console.log(x);
+
+//4.Use comments to describe the correct data type of the following variables:
+let length = 16; //number
+let lastName = 'Johnson'; //string
+
+//5
+function myFunction() {
+  alert('Hello World');
+}
+myFunction();
+
+//6
+var person = {
+  names: 'John',
+  age: 50,
+};
+alert(person.names + ' is ' + '' + person.age);
+
+//7
+
+//8(1)
+// let cars = ['volvo', 'jeep', 'Mercedes'];
+// let size = cars.length;
+
+//8(2)
+const Brand = ['Volvo', 'Jeep', 'Mercedes'];
+Brand.unshift('Ford');
+
+//9(1)
+let math = Math.random();
+
+//9(2)
+let z = Math.max(0, 10, 20);
+
+//9(3)
+let rt = Math.sqrt(9);
+console.log(rt);
+
+//10 (1)
+let a = 10;
+let b = 5;
+alert(a > b);
+
+//10(2)
+
+// let userAge = 18;
+// userAge < 18 ? console.log('you are young') : console.log('you are old');
+// const age = 19;
+// alert(age < 18 ? 'Too young' : 'Old enough');
